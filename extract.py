d3892325af669cc10505acfa1176c1fcbead893d
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2024-10-14 19:07:22 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -3,7 +3,15 @@
 import pandas as pd
 
 def extract_data(file_path):
-   
+    """
+    Extracts data from a CSV file.
+
+    Parameters:
+    - file_path (str): Path to the CSV file.
+
+    Returns:
+    - pd.DataFrame: Extracted data.
+    """
     """TODO"""
 
     return df
